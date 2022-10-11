98bcfbb356c1f96feb2905ca0da05a54617d0b10
Giovanna Melchiori <61929043+gmelchiori@users.noreply.github.com>
2022-10-10 22:53:13 -0300

    Add files via upload

---
+++ soma.java
@@ -0,0 +1,18 @@
+import java.util.*;  
+
+public class soma {
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);  
+        
+        System.out.print("Digite o primeiro número: ");  
+        int a= sc.nextInt();  
+        
+        System.out.print("Digite o segundo número: ");  
+        int b= sc.nextInt();  
+        
+        int c = a + b;
+        
+        System.out.println("Total da soma é " + c + ".\n");  
+    }  
+}  
